fb32dbf516f1051abb963d59114fb531ac6b4fc1
Sascha Tommasone <sascha@tommasone.me>
2023-09-14 13:02:11 +0200

    initial commit

---
+++ src/sha256/sha256.h
@@ -0,0 +1,20 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+#include <stdint.h>
+
+typedef struct sha256State {
+        uint32_t H[8];
+        uint32_t message[16];
+        uint32_t message_length;
+        uint64_t length;
+        uint8_t finalised;
+} sha256State_t;
+
+void initState(sha256State_t *state);
+
+void sha256(sha256State_t *state);
+
+void finalise(sha256State_t *state);
+
+#endif
