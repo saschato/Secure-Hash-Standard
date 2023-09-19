063fbd7c1c2f6bdf9eeff47de9129a5bf5450e08
Sascha Tommasone <sascha@tommasone.me>
2023-09-19 21:40:50 +0200

    smaller performance improvements

--- src/sha256/sha256.c
+++ src/sha256/sha256.c
@@ -13,15 +13,15 @@
 #define Maj(x,y,z) ((x & y) ^ (x & z) ^ (y & z))
 
 static inline uint32_t Sigma0(uint32_t x) {
-    	return ROTR(x,2) ^ ROTR(x,13) ^ ROTR(x,22);
+    return ROTR(x,2) ^ ROTR(x,13) ^ ROTR(x,22);
 }
 
 static inline uint32_t Sigma1(uint32_t x) {
-    	return ROTR(x,6) ^ ROTR(x,11) ^ ROTR(x,25);
+    return ROTR(x,6) ^ ROTR(x,11) ^ ROTR(x,25);
 }
 
 static inline uint32_t sigma0(uint32_t x) {
-    	return ROTR(x,7) ^ ROTR(x,18) ^ SHR(x,3);
+    return ROTR(x,7) ^ ROTR(x,18) ^ SHR(x,3);
 }
 
 static inline uint32_t sigma1(uint32_t x) {
@@ -30,13 +30,13 @@ static inline uint32_t sigma1(uint32_t x) {
 
 const uint32_t K[64] = {
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
 void initState(sha256State_t *state) {
@@ -63,57 +63,55 @@ void sha256Padding(sha256State_t *state) {
 	}
 
 	state->finalised = 1;
+	state->length *= 8;
 	state->message[14] = state->length >> 32;
 	state->message[15] = state->length & 0xffffffff;
 }
 
 void sha256(sha256State_t *state) {
-	uint32_t W[64] = {0}, T1, T2;
+	uint32_t T1, T2;
 	uint32_t a, b, c, d, e, f, g, h;
 
 	if (state->message_length != 64) {
 		sha256Padding(state);
 	}	
 	
-	for (int t = 0; t < 64; t++) {
-		if (t < 16) {
-			W[t] = state->message[t];
-		}
-		else {
-			W[t] = sigma1(W[t-2]) + W[t-7] + sigma0(W[t-15]) + W[t-16];
-		}
-	}
+	for (int t = 0; t < 16; t++) 
+		state->W[t] = state->message[t];
+
+	for (int t = 16; t < 64; t++) 
+		state->W[t] = sigma1(state->W[t-2]) + state->W[t-7] + sigma0(state->W[t-15]) + state->W[t-16];
 
 	a=state->H[0];
-    	b=state->H[1];
-    	c=state->H[2];
-    	d=state->H[3];
-    	e=state->H[4];
+    b=state->H[1];
+    c=state->H[2];
+    d=state->H[3];
+    e=state->H[4];
    	f=state->H[5];
    	g=state->H[6];
-    	h=state->H[7];
+    h=state->H[7];
 	
 	for (int t = 0; t < 64; t++) {
-        	T1 = h + Sigma1(e) + Ch(e,f,g) + K[t] + W[t];
-        	T2 = Sigma0(a) + Maj(a,b,c);
-        	h = g;
-        	g = f;
-        	f = e;
-        	e = d + T1;
-        	d = c;
-        	c = b;
-        	b = a;
-        	a = T1 + T2;
-    	}
+       	T1 = h + Sigma1(e) + Ch(e,f,g) + K[t] + state->W[t];
+       	T2 = Sigma0(a) + Maj(a,b,c);
+       	h = g;
+       	g = f;
+       	f = e;
+       	e = d + T1;
+       	d = c;
+       	c = b;
+       	b = a;
+       	a = T1 + T2;
+    }
 	
-	state->H[0] = a + state->H[0];
-    	state->H[1] = b + state->H[1];
-    	state->H[2] = c + state->H[2];
-    	state->H[3] = d + state->H[3];
-    	state->H[4] = e + state->H[4];
-    	state->H[5] = f + state->H[5];
-    	state->H[6] = g + state->H[6];
-    	state->H[7] = h + state->H[7];
+	state->H[0] += a;
+    state->H[1] += b;
+    state->H[2] += c;
+    state->H[3] += d;
+    state->H[4] += e;
+    state->H[5] += f;
+    state->H[6] += g;
+    state->H[7] += h;
 }
 
 void finalise(sha256State_t *state) {
